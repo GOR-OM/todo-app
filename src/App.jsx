45151e5041d7dde2540601e3bfba20ba0eaf8cbd
GOR-OM <202101484@daiict.ac.in>
2023-12-21 19:11:47 +0530

    create Logi .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from 'react'
 import './styles/App.scss'
-
+import  Home  from './pages/Home'
+import Header from './component/Header'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Profile from './pages/Profile'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+
+
+
+        </Routes>
+      </Router>
     </>
   )
 }
